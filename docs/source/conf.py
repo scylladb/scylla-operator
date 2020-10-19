9662720dcdf1c67c00ea25e29409cc4d2e65ab10
lauranovich <laura@scylladb.com>
2020-10-19 14:32:18 +0300

    change base URL

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -130,7 +130,7 @@ html_sidebars = {'**': ['side-nav.html']}
 htmlhelp_basename = 'ScyllaDocumentationdoc'
 
 # URL which points to the root of the HTML documentation. 
-html_baseurl = 'https://scylladb.github.io/sphinx-scylladb-theme'
+html_baseurl = 'https://scylladb.github.io/scylla-operator'
 
 # Dictionary of values to pass into the template engine’s context for all pages
 html_context = {'html_baseurl': html_baseurl}

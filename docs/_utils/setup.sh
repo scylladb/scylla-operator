5908083e761b1c3a22015ad4b1c86637b1f2b1a9
dgarcia360 <dgarcia360@outlook.com>
2020-11-09 18:36:40 +0100

    docs: update theme automatically

--- docs/_utils/setup.sh
+++ docs/_utils/setup.sh
@@ -13,3 +13,4 @@ fi
 which python3 || { echo "Failed to find python3. Try installing Python for your operative system: https://www.python.org/downloads/" && exit 1; }
 which poetry || curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - && source ${HOME}/.poetry/env
 poetry install
+poetry update

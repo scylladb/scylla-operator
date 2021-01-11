a275c9d2e8d77d9610c9439a4a49fe93364906a8
Maciej Zimnoch <maciej@scylladb.com>
2021-01-11 12:06:11 +0100

    docs: publish v1.0.0 docs as latest

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -152,7 +152,7 @@ redirects_file = "_utils/redirections.yaml"
 
 # -- Options for multiversion extension ----------------------------------
 # Whitelist pattern for tags (set to None to ignore all tags)
-TAGS = []
+TAGS = ['v0.3.0', 'v1.0.0$']
 smv_tag_whitelist = multiversion_regex_builder(TAGS)
 # Whitelist pattern for branches (set to None to ignore all branches)
 BRANCHES = ['master']

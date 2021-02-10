77fac2f26d50727cfd4b4228a1075bd8ee703b0d
Tomas Nozicka <tomas.nozicka@scylladb.com>
2021-02-10 17:24:23 +0100

    Fix CI promotion and few nits

--- hack/lib/tag-from-gh-ref.sh
+++ hack/lib/tag-from-gh-ref.sh
@@ -9,11 +9,11 @@ function tag_from_gh_ref {
     echo "${1}" > "${tmp_file}"
     (
         # master
-        sed -i -E -e '/^master$/,${s//latest/;b};$q1' "${tmp_file}" || \
+        sed -i -E -e '/^refs\/heads\/master$/,${s//latest/;b};$q1' "${tmp_file}" || \
         # branch name
-        sed -i -E -e '/^v([0-9]+\.[0-9]+)$/,${s//\1/;b};$q1' "${tmp_file}" || \
+        sed -i -E -e '/^refs\/heads\/v([0-9]+\.[0-9]+)$/,${s//\1/;b};$q1' "${tmp_file}" || \
         # tag name
-        sed -i -E -e '/^v([0-9]+\.[0-9]+\.[0-9]+-(alpha|beta|rc)\.[0-9]+)$/,${s//\1/;b};$q1' "${tmp_file}" || \
+        sed -i -E -e '/^refs\/tags\/v([0-9]+\.[0-9]+\.[0-9]+-(alpha|beta|rc)\.[0-9]+)$/,${s//\1/;b};$q1' "${tmp_file}" || \
         false
     ) && cat "${tmp_file}"
 }
@@ -32,18 +32,22 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 
     ( (tag_from_gh_ref '' && exit 1) || true )
     ( (tag_from_gh_ref 'foo' && exit 1) || true )
-    ( (tag_from_gh_ref 'masters' && exit 1) || true )
+    ( (tag_from_gh_ref 'master' && exit 1) || true )
+    ( (tag_from_gh_ref '/refs/heads/master' && exit 1) || true )
+    ( (tag_from_gh_ref 'refs/heads/masters' && exit 1) || true )
     # Fail on final tags, those shouldn't be rebuild by CI but instead promoted from a previous tested image.
+    ( (tag_from_gh_ref 'refs/tags/v1.0.0' && exit 1) || true )
     ( (tag_from_gh_ref 'v1.0.0' && exit 1) || true )
-    ( (tag_from_gh_ref 'v1.0.0-alpha' && exit 1) || true )
-    ( (tag_from_gh_ref 'v1.0.0-alpha.' && exit 1) || true )
-    ( (tag_from_gh_ref 'v1.0.0-alpha0' && exit 1) || true )
-    ( (tag_from_gh_ref 'v1.0.0.0' && exit 1) || true )
-    [[ "$( tag_from_gh_ref 'master' )" == 'latest' ]]
-    [[ "$( tag_from_gh_ref 'v1.0' )" == '1.0' ]]
-    [[ "$( tag_from_gh_ref 'v1.0.0-alpha.0' )" == '1.0.0-alpha.0' ]]
-    [[ "$( tag_from_gh_ref 'v1.0.0-beta.0' )" == '1.0.0-beta.0' ]]
-    [[ "$( tag_from_gh_ref 'v1.0.0-rc.0' )" == '1.0.0-rc.0' ]]
+    ( (tag_from_gh_ref 'refs/tags/v1.0.0-alpha' && exit 1) || true )
+    ( (tag_from_gh_ref 'refs/tags/v1.0.0-alpha.' && exit 1) || true )
+    ( (tag_from_gh_ref 'refs/tags/v1.0.0-alpha0' && exit 1) || true )
+    ( (tag_from_gh_ref 'refs/tags/v1.0.0.0' && exit 1) || true )
+    ( (tag_from_gh_ref 'refs/heads/v1.0.0.0' && exit 1) || true )
+    [[ "$( tag_from_gh_ref 'refs/heads/master' )" == 'latest' ]]
+    [[ "$( tag_from_gh_ref 'refs/heads/v1.0' )" == '1.0' ]]
+    [[ "$( tag_from_gh_ref 'refs/tags/v1.0.0-alpha.0' )" == '1.0.0-alpha.0' ]]
+    [[ "$( tag_from_gh_ref 'refs/tags/v1.0.0-beta.0' )" == '1.0.0-beta.0' ]]
+    [[ "$( tag_from_gh_ref 'refs/tags/v1.0.0-rc.0' )" == '1.0.0-rc.0' ]]
 
     echo "${message_prefix}..SUCCESS." >&2
-fi;
+fi

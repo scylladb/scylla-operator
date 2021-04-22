a4d029c9c155ba6eb8d6223bc87e9ebb8b5351dc
Maciej Zimnoch <maciej@scylladb.com>
2021-04-22 12:36:02 +0200

    e2e: Added smoke test for Scylla Manager integration (#478)

---
+++ test/e2e/set/scyllacluster/scyllamanager.go
@@ -0,0 +1,104 @@
+// Copyright (C) 2021 ScyllaDB
+
+package scyllacluster
+
+import (
+	"context"
+	"encoding/json"
+
+	g "github.com/onsi/ginkgo"
+	o "github.com/onsi/gomega"
+	scyllav1 "github.com/scylladb/scylla-operator/pkg/api/scylla/v1"
+	scyllaclusterfixture "github.com/scylladb/scylla-operator/test/e2e/fixture/scyllacluster"
+	"github.com/scylladb/scylla-operator/test/e2e/framework"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = g.Describe("Scylla Manager integration", func() {
+	defer g.GinkgoRecover()
+
+	f := framework.NewFramework("scyllacluster")
+
+	g.It("should discover cluster and sync tasks", func() {
+		ctx, cancel := context.WithTimeout(context.Background(), testTimout)
+		defer cancel()
+
+		sc := scyllaclusterfixture.BasicScyllaCluster.ReadOrFail()
+		sc.Spec.Datacenter.Racks[0].Members = 1
+		sc.Spec.Repairs = append(sc.Spec.Repairs, scyllav1.RepairTaskSpec{
+			SchedulerTaskSpec: scyllav1.SchedulerTaskSpec{
+				Name:     "weekly repair",
+				Interval: "7d",
+			},
+			Parallel: 123,
+		})
+
+		// Backup scheduling is going to fail because location is not accessible in our env.
+		sc.Spec.Backups = append(sc.Spec.Backups, scyllav1.BackupTaskSpec{
+			SchedulerTaskSpec: scyllav1.SchedulerTaskSpec{
+				Name:     "daily backup",
+				Interval: "1d",
+			},
+			Location: []string{"s3:bucket"},
+		})
+
+		framework.By("Creating a ScyllaCluster")
+		err := framework.SetupScyllaClusterSA(ctx, f.KubeClient().CoreV1(), f.KubeClient().RbacV1(), f.Namespace(), sc.Name)
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		sc, err = f.ScyllaClient().ScyllaV1().ScyllaClusters(f.Namespace()).Create(ctx, sc, metav1.CreateOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		framework.By("Waiting for the ScyllaCluster to deploy")
+		waitCtx1, waitCtx1Cancel := contextForRollout(ctx, sc)
+		defer waitCtx1Cancel()
+		sc, err = waitForScyllaClusterState(waitCtx1, f.ScyllaClient().ScyllaV1(), sc.Namespace, sc.Name, scyllaClusterRolledOut)
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		verifyScyllaCluster(ctx, f.KubeClient(), sc)
+
+		framework.By("Waiting for the cluster sync with Scylla Manager")
+		registeredInManagerCond := func(sc *scyllav1.ScyllaCluster) (bool, error) {
+			return sc.Status.ManagerID != nil, nil
+		}
+
+		repairTaskScheduledCond := func(cluster *scyllav1.ScyllaCluster) (bool, error) {
+			for _, r := range cluster.Status.Repairs {
+				if r.Name == sc.Spec.Repairs[0].Name {
+					return r.ID != "", nil
+				}
+			}
+			return false, nil
+		}
+
+		backupTaskSyncFailedCond := func(cluster *scyllav1.ScyllaCluster) (bool, error) {
+			for _, b := range cluster.Status.Backups {
+				if b.Name == sc.Spec.Backups[0].Name {
+					return b.Error != "", nil
+				}
+			}
+			return false, nil
+		}
+
+		waitCtx2, waitCtx2Cancel := contextForManagerSync(ctx, sc)
+		defer waitCtx2Cancel()
+		conditions := []func(cluster *scyllav1.ScyllaCluster) (bool, error){
+			registeredInManagerCond,
+			repairTaskScheduledCond,
+			backupTaskSyncFailedCond,
+		}
+		sc, err = waitForScyllaClusterState(waitCtx2, f.ScyllaClient().ScyllaV1(), sc.Namespace, sc.Name, conditions...)
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		managerClient, err := getManagerClient(ctx, f.KubeAdminClient().CoreV1())
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		framework.By("Verifying that task properties were synchronized")
+		tasks, err := managerClient.ListTasks(ctx, *sc.Status.ManagerID, "repair", false, "")
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(tasks.ExtendedTaskSlice).To(o.HaveLen(1))
+		repairTask := tasks.ExtendedTaskSlice[0]
+		o.Expect(repairTask.Name).To(o.Equal(sc.Spec.Repairs[0].Name))
+		o.Expect(repairTask.Properties.(map[string]interface{})["parallel"].(json.Number).Int64()).To(o.Equal(int64(123)))
+	})
+})

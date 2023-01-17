ef2b19b15f52ef1976e05cee590797e19006dd1a
Maciej Zimnoch <maciej@scylladb.com>
2023-01-17 16:46:44 +0100

    Fix support for immutable fields in Job Apply
    Recent change adding generic apply function (#1138), introduced a regression
    in Apply function for batchv1.Jobs. Immutable fields weren't verified
    and delete/create logic wasn't triggered. Our NodeConfig controllers are
    changing immutable Pod Template when a new ScyllaDB Pod lands on a Node.

--- pkg/resourceapply/batch.go
+++ pkg/resourceapply/batch.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	batchv1 "k8s.io/api/batch/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
 	batchv1client "k8s.io/client-go/kubernetes/typed/batch/v1"
 	batchv1listers "k8s.io/client-go/listers/batch/v1"
 	"k8s.io/client-go/tools/record"
@@ -16,7 +17,32 @@ func ApplyJobWithControl(
 	required *batchv1.Job,
 	options ApplyOptions,
 ) (*batchv1.Job, bool, error) {
-	return ApplyGeneric[*batchv1.Job](ctx, control, recorder, required, options)
+	return ApplyGenericWithHandlers[*batchv1.Job](
+		ctx,
+		control,
+		recorder,
+		required,
+		options,
+		nil,
+		func(required *batchv1.Job, existing *batchv1.Job) string {
+			if !equality.Semantic.DeepEqual(existing.Spec.Completions, required.Spec.Completions) {
+				return "spec.completions is immutable"
+			}
+			if !equality.Semantic.DeepEqual(existing.Spec.Selector, required.Spec.Selector) {
+				return "spec.selector is immutable"
+			}
+			if !equality.Semantic.DeepEqual(existing.Spec.Template, required.Spec.Template) {
+				return "spec.template is immutable"
+			}
+			if !equality.Semantic.DeepEqual(existing.Spec.CompletionMode, required.Spec.CompletionMode) {
+				return "spec.completionMode is immutable"
+			}
+			if !equality.Semantic.DeepEqual(existing.Spec.PodFailurePolicy, required.Spec.PodFailurePolicy) {
+				return "spec.podFailurePolicy is immutable"
+			}
+			return ""
+		},
+	)
 }
 
 func ApplyJob(

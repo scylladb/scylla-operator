85bb4d4070ac558804bffc0a734a698c9bd5403a
Kacper Rzetelski <kacper.rzetelski@scylladb.com>
2023-08-22 16:27:02 +0200

    Support full grammar of named and tagged image references

--- pkg/naming/names.go
+++ pkg/naming/names.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/containers/image/v5/docker/reference"
 	"github.com/pkg/errors"
 	scyllav1 "github.com/scylladb/scylla-operator/pkg/api/scylla/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -101,11 +102,16 @@ func IndexFromName(n string) (int32, error) {
 
 // ImageToVersion strips version part from container image.
 func ImageToVersion(image string) (string, error) {
-	parts := strings.Split(image, ":")
-	if len(parts) != 2 || len(parts[1]) == 0 {
-		return "", errors.New(fmt.Sprintf("Invalid image name: %s", image))
+	named, err := reference.ParseNormalizedNamed(image)
+	if err != nil {
+		return "", fmt.Errorf("can't parse image: %w", err)
 	}
-	return parts[1], nil
+	tagged, ok := named.(reference.NamedTagged)
+	if !ok {
+		return "", fmt.Errorf("invalid, non-tagged image reference of type %T: %s", named, image)
+	}
+
+	return tagged.Tag(), nil
 }
 
 // FindScyllaContainer returns Scylla container from given list.

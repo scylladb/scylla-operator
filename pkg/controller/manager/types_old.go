6d4f114007e78f51d2457274967f0d691a7624a6
Kacper Rzetelski <rzetelski.k@gmail.com>
2024-03-19 23:58:20 +0100

    Support cron and timezone API fields in manager task conversion funcs

--- pkg/controller/manager/types_old.go
+++ pkg/controller/manager/types_old.go
@@ -3,6 +3,7 @@
 package manager
 
 import (
+	"fmt"
 	"math"
 	"regexp"
 	"strconv"
@@ -21,7 +22,7 @@ import (
 
 type RepairTask v1.RepairTaskStatus
 
-func (r RepairTask) ToManager() (*managerclient.Task, error) {
+func (r *RepairTask) ToManager() (*managerclient.Task, error) {
 	t := &managerclient.Task{
 		ID:         r.ID,
 		Type:       "repair",
@@ -37,16 +38,18 @@ func (r RepairTask) ToManager() (*managerclient.Task, error) {
 		return nil, errors.Wrap(err, "parse start date")
 	}
 	t.Schedule.StartDate = &startDate
-
-	if _, err := duration.ParseDuration(r.Interval); err != nil {
-		return nil, errors.Wrap(err, "parse interval")
-	}
 	t.Schedule.Interval = r.Interval
 
 	if r.NumRetries != nil {
-		t.Schedule.NumRetries = int64(*r.NumRetries)
+		t.Schedule.NumRetries = *r.NumRetries
 	}
 
+	if r.Cron != nil {
+		t.Schedule.Cron = *r.Cron
+	}
+	if r.Timezone != nil {
+		t.Schedule.Timezone = *r.Timezone
+	}
 	if r.Keyspace != nil {
 		props["keyspace"] = unescapeFilters(r.Keyspace)
 	}
@@ -86,10 +89,14 @@ func (r *RepairTask) FromManager(t *managerclient.TaskListItem) error {
 	r.Interval = t.Schedule.Interval
 	r.StartDate = t.Schedule.StartDate.String()
 	r.NumRetries = pointer.Ptr(t.Schedule.NumRetries)
+	r.Cron = pointer.Ptr(t.Schedule.Cron)
+	r.Timezone = pointer.Ptr(t.Schedule.Timezone)
 
-	props := t.Properties.(map[string]interface{})
-	if err := mapstructure.Decode(props, r); err != nil {
-		return errors.Wrap(err, "decode properties")
+	if t.Properties != nil {
+		props := t.Properties.(map[string]interface{})
+		if err := mapstructure.Decode(props, r); err != nil {
+			return fmt.Errorf("can't decode properties: %w", err)
+		}
 	}
 
 	return nil
@@ -105,7 +112,7 @@ func (r *RepairTask) SetStartDate(sd string) {
 
 type BackupTask v1.BackupTaskStatus
 
-func (b BackupTask) ToManager() (*managerclient.Task, error) {
+func (b *BackupTask) ToManager() (*managerclient.Task, error) {
 	t := &managerclient.Task{
 		ID:         b.ID,
 		Type:       "backup",
@@ -121,14 +128,17 @@ func (b BackupTask) ToManager() (*managerclient.Task, error) {
 		return nil, errors.Wrap(err, "parse start date")
 	}
 	t.Schedule.StartDate = &startDate
-
-	if _, err := duration.ParseDuration(b.Interval); err != nil {
-		return nil, errors.Wrap(err, "parse interval")
-	}
 	t.Schedule.Interval = b.Interval
 
 	if b.NumRetries != nil {
-		t.Schedule.NumRetries = int64(*b.NumRetries)
+		t.Schedule.NumRetries = *b.NumRetries
+	}
+
+	if b.Cron != nil {
+		t.Schedule.Cron = *b.Cron
+	}
+	if b.Timezone != nil {
+		t.Schedule.Timezone = *b.Timezone
 	}
 
 	if b.Keyspace != nil {
@@ -149,6 +159,7 @@ func (b BackupTask) ToManager() (*managerclient.Task, error) {
 	}
 
 	props["location"] = b.Location
+
 	t.Name = b.Name
 	t.Properties = props
 
@@ -161,10 +172,14 @@ func (b *BackupTask) FromManager(t *managerclient.TaskListItem) error {
 	b.Interval = t.Schedule.Interval
 	b.StartDate = t.Schedule.StartDate.String()
 	b.NumRetries = pointer.Ptr(t.Schedule.NumRetries)
+	b.Cron = pointer.Ptr(t.Schedule.Cron)
+	b.Timezone = pointer.Ptr(t.Schedule.Timezone)
 
-	props := t.Properties.(map[string]interface{})
-	if err := mapstructure.Decode(props, b); err != nil {
-		return errors.Wrap(err, "decode properties")
+	if t.Properties != nil {
+		props := t.Properties.(map[string]interface{})
+		if err := mapstructure.Decode(props, b); err != nil {
+			return fmt.Errorf("can't decode properties: %w", err)
+		}
 	}
 
 	return nil

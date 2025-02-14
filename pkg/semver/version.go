6d7a6f4ab4be4131d08100a7587184220e70e56e
grzywin <kamil.grzywinski@scylladb.com>
2025-02-14 13:34:04 +0100

    Add support of names and digest ScyllaDB image reference

--- pkg/semver/version.go
+++ pkg/semver/version.go
@@ -17,7 +17,23 @@ limitations under the License.
 package semver
 
 import (
+	"context"
+	"fmt"
+	"regexp"
+	"strings"
+	"sync"
+
 	"github.com/blang/semver"
+	"github.com/containers/image/v5/docker"
+	"github.com/containers/image/v5/image"
+	"github.com/containers/image/v5/manifest"
+	"github.com/containers/image/v5/types"
+	"k8s.io/klog/v2"
+)
+
+var (
+	cacheScyllaDBImageVersionAndDigest     = make(map[string]string)
+	cacheLockScyllaDBImageVersionAndDigest sync.RWMutex
 )
 
 var (
@@ -46,4 +62,99 @@ func (sv ScyllaVersion) SupportFeatureUnsafe(featureVersion semver.Version) bool
 // SupportFeatureSafe return true if a feature is supported (and always false if the version is unknown)
 func (sv ScyllaVersion) SupportFeatureSafe(featureVersion semver.Version) bool {
 	return !sv.unknown && sv.version.GTE(featureVersion)
+}
+
+func GetImageVersionAndDigest(imageName, version string) (string, error) {
+	// This is for case when we pass manually the image with digest
+	if strings.Contains(version, "@") {
+		return version, nil
+	}
+
+	imageReference := fmt.Sprintf("scylladb/%s", imageName)
+
+	if strings.Contains(version, ":") && !strings.Contains(version, "@") {
+		imageReference = fmt.Sprintf("%s@%s", imageReference, version)
+	} else {
+		imageReference = fmt.Sprintf("%s:%s", imageReference, version)
+	}
+
+	cacheLockScyllaDBImageVersionAndDigest.RLock()
+	cached, ok := cacheScyllaDBImageVersionAndDigest[imageReference]
+	cacheLockScyllaDBImageVersionAndDigest.RUnlock()
+
+	if ok {
+		return cached, nil
+	}
+
+	ctx := context.Background()
+	transport := docker.Transport
+
+	ref, err := transport.ParseReference(fmt.Sprintf("//%s", imageReference))
+	if err != nil {
+		return "", fmt.Errorf("can't parse image reference: %w", err)
+	}
+
+	sysCtx := &types.SystemContext{}
+
+	src, err := ref.NewImageSource(ctx, sysCtx)
+	if err != nil {
+		return "", fmt.Errorf("can't get new image source: %w", err)
+	}
+	defer func() {
+		if closeErr := src.Close(); closeErr != nil {
+			klog.ErrorS(closeErr, "failed to close image source")
+		}
+	}()
+
+	img, err := image.FromUnparsedImage(ctx, sysCtx, image.UnparsedInstance(src, nil))
+	if err != nil {
+		return "", fmt.Errorf("can't read unparsed image: %w", err)
+	}
+
+	inspect, err := img.Inspect(ctx)
+	if err != nil {
+		return "", fmt.Errorf("can't inspect image: %w", err)
+	}
+
+	versionLabel, ok := inspect.Labels["org.opencontainers.image.version"]
+	if !ok {
+		klog.Warningf("no org.opencontainers.image.version label found for image: %s", imageReference)
+		return version, nil
+	}
+
+	manifestBytes, _, err := img.Manifest(ctx)
+	if err != nil {
+		return "", fmt.Errorf("can't get image manifest: %w", err)
+	}
+
+	digest, err := manifest.Digest(manifestBytes)
+	if err != nil {
+		return "", fmt.Errorf("can't compute digest: %w", err)
+	}
+
+	resolvedVersion := fmt.Sprintf("%s@%s", versionLabel, digest)
+	klog.V(4).InfoS("Full image version", "imageName", imageName, "resolvedVersion", resolvedVersion)
+
+	reBaseVersion := regexp.MustCompile(`\d+\.\d+\.\d+`)
+	baseVersion := reBaseVersion.FindString(resolvedVersion)
+
+	// When we pass only digest, it might refer to the RC version of the image. It will have the following form:
+	// 2025.1.0~rc2-0.20250216.abcdef
+	// We need to extract the "base" version from it, which is: 2025.1.0-rc2
+	reSuffix := regexp.MustCompile(`~([^\-]+)-`)
+	suffixMatch := reSuffix.FindStringSubmatch(resolvedVersion)
+	if len(suffixMatch) > 1 {
+		baseVersion = fmt.Sprintf("%s-%s", baseVersion, suffixMatch[1])
+	}
+
+	if baseVersion == "" {
+		klog.Warningf("could not extract semantic version from: %s", resolvedVersion)
+		return versionLabel, nil
+	}
+
+	cacheLockScyllaDBImageVersionAndDigest.Lock()
+	cacheScyllaDBImageVersionAndDigest[imageReference] = baseVersion
+	cacheLockScyllaDBImageVersionAndDigest.Unlock()
+
+	return baseVersion, nil
 }

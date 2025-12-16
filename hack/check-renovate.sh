2706b1018af16e503db2f9ccb5f23e585d3c583d
Grzegorz Burzyński <grzegorz.burzynski@scylladb.com>
2025-12-16 15:40:12 +0100

    Add initial Renovate configuration for updating Scylla

---
+++ hack/check-renovate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This scripts runs renovate in dry-run mode against the local config.
+# It can be used to check if changes to the config result in the expected updates.
+
+# Note: Renovate CLI output is quote verbose, so it's recommended to redirect it to a file for easier analysis.
+
+set -euxEo pipefail
+shopt -s inherit_errexit
+
+npx --yes renovate --platform=local --dry-run=full --print-config=true

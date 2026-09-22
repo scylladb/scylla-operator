dbd1a6d642fc72299c30da017031122a03eaafdf
Kacper Rzetelski <kacper.rzetelski@scylladb.com>
2026-09-22 15:33:48 +0200

    ci: run the e2e suites as periodics on GHA

--- hack/.ci/lib/e2e.sh
+++ hack/.ci/lib/e2e.sh
@@ -307,7 +307,7 @@ function run-e2e {
   )
 
   gcs_sa_in_container_path=""
-  if [[ -n "${SO_GCS_SERVICE_ACCOUNT_CREDENTIALS_PATH+x}" ]]; then
+  if [[ -n "${SO_GCS_SERVICE_ACCOUNT_CREDENTIALS_PATH:-}" ]]; then
     gcs_sa_in_container_path=/var/run/secrets/gcs-service-account-credentials/gcs-service-account.json
     kubectl create -n=e2e secret generic gcs-service-account-credentials --from-file=gcs-service-account.json="${SO_GCS_SERVICE_ACCOUNT_CREDENTIALS_PATH}" --dry-run=client -o=yaml | kubectl_create -f=-
   else
